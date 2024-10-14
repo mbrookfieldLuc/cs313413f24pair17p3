53cfcc335cb00c2c91bdb877118a8d456cc5a847
milesbrookfield <119908104+milesbrookfield@users.noreply.github.com>
2024-10-14 14:17:38 -0500

    More work - count tests all passing

--- app/src/main/java/edu/luc/etl/cs313/android/shapes/model/Count.java
+++ app/src/main/java/edu/luc/etl/cs313/android/shapes/model/Count.java
@@ -36,7 +36,7 @@ public class Count implements Visitor<Integer> {
 
     @Override
     public Integer onOutline(final Outline o) {
-        return -1;
+        return o.getShape().accept(this);
     }
 
     @Override
